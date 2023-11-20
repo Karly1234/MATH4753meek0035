7a18753b270966c050e1754eb51f83f1d7081b61
Karly1234 <karlyrose0309@gmail.com>
2023-11-20 16:16:14 -0600

    This is the fire dataset

---
+++ R/fire.R
@@ -0,0 +1,12 @@
+#' Fire Damage
+#'
+#' A data set collected concerning hone damage from fire
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'    \item{DISTANCE}{distance from fire station in miles}
+#'    \item{DAMAGE}{X 1000 dollars in damage}
+#' }
+#' @source \url{https:www.crcpress.com}
+#'
+"fire"
